398fc41d44f608865af4f3db2750251d62660e99
Håkon Malmedal <hmalmedal@gmail.com>
2015-04-09 12:13:15 +0200

    Beregner endring ett sted

--- mandater.R
+++ mandater.R
@@ -2,6 +2,9 @@ library(readr)
 library(dplyr, warn.conflicts = FALSE)
 source('saintelague.R')
 
+pre2004 <- c(8, 15, 16, 8, 7, 7, 8, 6, 4, 5, 11, 17, 5, 10, 10, 6, 12, 6, 4)
 Mandatfordelingsdata <- read_csv("Mandatfordelingsdata.csv") %>%
   group_by(År) %>%
-  mutate(Mandater = saintelague(169, Areal * 1.8 + Folketall))
+  mutate(Mandater = saintelague(169, Areal * 1.8 + Folketall)) %>%
+  ungroup() %>%
+  mutate(Endring = diff(c(pre2004, Mandater), lag = 19))
