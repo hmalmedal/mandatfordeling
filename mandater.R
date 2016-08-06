00b252b2f17008859db799c432257ceb83724db6
Håkon Malmedal <hmalmedal@gmail.com>
2016-08-06 15:31:24 +0200

    col_types

--- mandater.R
+++ mandater.R
@@ -3,7 +3,8 @@ library(dplyr)
 source("saintelague.R")
 
 pre2004 <- c(8, 15, 16, 8, 7, 7, 8, 6, 4, 5, 11, 17, 5, 10, 10, 6, 12, 6, 4)
-mandatfordelingsdata <- read_csv("mandatfordelingsdata.csv") %>%
+mandatfordelingsdata <- read_csv("mandatfordelingsdata.csv",
+                                 col_types = "ciii") %>%
   group_by(År) %>%
   mutate(Mandater = saintelague(169, Areal * 1.8 + Folketall)) %>%
   ungroup() %>%
