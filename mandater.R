8544c07d73ea822c7192bc68456863e0b7dddea1
Håkon Malmedal <hmalmedal@gmail.com>
2018-02-28 11:05:58 +0100

    Heltall

--- mandater.R
+++ mandater.R
@@ -2,7 +2,8 @@ library(readr)
 library(dplyr)
 source("saintelague.R")
 
-pre2004 <- c(8, 15, 16, 8, 7, 7, 8, 6, 4, 5, 11, 17, 5, 10, 10, 6, 12, 6, 4)
+pre2004 <- c(8, 15, 16, 8, 7, 7, 8, 6, 4, 5, 11, 17, 5, 10, 10, 6, 12, 6, 4) %>%
+  as.integer()
 mandatfordelingsdata <- read_csv("mandatfordelingsdata.csv",
                                  col_types = "ciii") %>%
   group_by(År) %>%
--- saintelague.R
+++ saintelague.R
@@ -1,5 +1,5 @@
 saintelague <- function(votes, available_seats, modifier = 1) {
-  seats <- rep(0, length(votes))
+  seats <- integer(length(votes))
 
   for (j in seq_len(available_seats)) {
     divisors <- 2 * seats + 1
@@ -8,7 +8,7 @@ saintelague <- function(votes, available_seats, modifier = 1) {
     quotients <- votes / divisors
 
     i <- which.max(quotients)
-    seats[i] <- seats[i] + 1
+    seats[i] <- seats[i] + 1L
   }
 
   seats
